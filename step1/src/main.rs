0d17087a4a72da36ca4beb87cba5241924370741
Asami Doi <doiasami1219@gmail.com>
2020-05-12 21:50:49 +0900

    Udpate the way to add 2 values.

--- step1/src/main.rs
+++ step1/src/main.rs
@@ -66,11 +66,11 @@ impl Cpu {
             0x13 => {
                 // addi
                 let imm = ((inst & 0xfff00000) as i32 as i64 >> 20) as u64;
-                self.regs[rd] = self.regs[rs1] + imm;
+                self.regs[rd] = self.regs[rs1].wrapping_add(imm);
             }
             0x33 => {
                 // add
-                self.regs[rd] = self.regs[rs1] + self.regs[rs2];
+                self.regs[rd] = self.regs[rs1].wrapping_add(self.regs[rs2]);
             }
             _ => {
                 dbg!(format!("not implemented yet: opcode {:#x}", opcode));

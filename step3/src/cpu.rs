1064fefd9028823483f00de842fe407d71bd7b90
Asami Doi <doiasami1219@gmail.com>
2020-06-29 21:41:09 +0900

    Add constants for csr addresses.

--- step3/src/cpu.rs
+++ step3/src/cpu.rs
@@ -3,6 +3,46 @@
 /// Default memory size (128MiB).
 pub const MEMORY_SIZE: u64 = 1024 * 1024 * 128;
 
+// Machine-level CSRs.
+/// Hardware thread ID.
+pub const MHARTID: usize = 0xf14;
+/// Machine status register.
+pub const MSTATUS: usize = 0x300;
+/// Machine exception delefation register.
+pub const MEDELEG: usize = 0x302;
+/// Machine interrupt delefation register.
+pub const MIDELEG: usize = 0x303;
+/// Machine interrupt-enable register.
+pub const MIE: usize = 0x304;
+/// Machine trap-handler base address.
+pub const MTVEC: usize = 0x305;
+/// Machine counter enable.
+pub const MCOUNTEREN: usize = 0x306;
+/// Scratch register for machine trap handlers.
+pub const MSCRATCH: usize = 0x340;
+/// Machine exception program counter.
+pub const MEPC: usize = 0x341;
+
+// Supervisor-level CSRs.
+/// Supervisor status register.
+pub const SSTATUS: usize = 0x100;
+/// Supervisor interrupt-enable register.
+pub const SIE: usize = 0x104;
+/// Supervisor trap handler base address.
+pub const STVEC: usize = 0x105;
+/// Scratch register for supervisor trap handlers.
+pub const SSCRATCH: usize = 0x140;
+/// Supervisor exception program counter.
+pub const SEPC: usize = 0x141;
+/// Supervisor trap cause.
+pub const SCAUSE: usize = 0x142;
+/// Supervisor bad address or instruction.
+pub const STVAL: usize = 0x143;
+/// Supervisor interrupt pending.
+pub const SIP: usize = 0x144;
+/// Supervisor address translation and protection.
+pub const SATP: usize = 0x180;
+
 /// The CPU to contain registers, a program coutner, and memory.
 pub struct Cpu {
     /// 32 64-bit integer registers.
@@ -52,7 +92,7 @@ impl Cpu {
                     i + 3,
                     self.regs[i + 3],
                 )
-            );
+                );
         }
         println!("{}", output);
     }

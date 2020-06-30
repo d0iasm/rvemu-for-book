006cad49ed41e2b7898752a11bd522661a75efc4
Asami Doi <doiasami1219@gmail.com>
2020-06-30 21:51:07 +0900

    Add a step4 project that supports mret and sret instructions.

---
+++ step4/src/main.rs
@@ -0,0 +1,47 @@
+mod cpu;
+
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+use crate::cpu::*;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Usage: rvemu-simple <filename>");
+    }
+    let mut file = File::open(&args[1])?;
+    let mut binary = Vec::new();
+    file.read_to_end(&mut binary)?;
+
+    let mut cpu = Cpu::new(binary);
+
+    while cpu.pc < cpu.memory.len() as u64 {
+        // 1. Fetch.
+        let inst = cpu.fetch();
+
+        // 2. Add 4 to the program counter.
+        cpu.pc += 4;
+
+        // 3. Decode.
+        // 4. Execute.
+        match cpu.execute(inst) {
+            // True if an error happens.
+            true => break,
+            false => {}
+        };
+
+        // This is a workaround for avoiding an infinite loop.
+        if cpu.pc == 0 {
+            break;
+        }
+    }
+    cpu.dump_registers();
+    println!("-----------------------------------------------------------------------------------------------------------");
+    cpu.dump_csrs();
+
+    Ok(())
+}

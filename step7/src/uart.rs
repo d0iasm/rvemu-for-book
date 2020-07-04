1eb08abb7f112d3a9b1e02a86654fbaa86a037c4
Asami Doi <doiasami1219@gmail.com>
2020-07-04 20:07:48 +0900

    Move echoback test files to step7 from step8.

--- step7/src/uart.rs
+++ step7/src/uart.rs
@@ -6,6 +6,11 @@
 
 use std::io;
 use std::io::prelude::*;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, Condvar, Mutex,
+};
+use std::thread;
 
 use crate::bus::*;
 use crate::trap::*;
@@ -31,10 +36,10 @@ pub const UART_LSR_RX: u8 = 1;
 pub const UART_LSR_TX: u8 = 1 << 5;
 
 pub struct Uart {
-    /// 8-bit buffer for input and output.
-    buffer: u8,
-    /// Line status register.
-    lsr: u8,
+    /// Pair of an array for UART buffer and a conditional variable.
+    uart: Arc<(Mutex<[u8; UART_SIZE as usize]>, Condvar)>,
+    /// Bit if an interrupt happens.
+    interrupting: Arc<AtomicBool>,
 }
 
 impl Device for Uart {
@@ -56,26 +61,64 @@ impl Device for Uart {
 impl Uart {
     /// Create a new `Uart` object.
     pub fn new() -> Self {
-        Self { buffer: 0, lsr: 0 }
+        let uart = Arc::new((Mutex::new([0; UART_SIZE as usize]), Condvar::new()));
+        let interrupting = Arc::new(AtomicBool::new(false));
+        {
+            let (uart, _cvar) = &*uart;
+            let mut uart = uart.lock().expect("failed to get an UART object");
+            // Transmitter hold register is empty.
+            uart[(UART_LSR - UART_BASE) as usize] |= UART_LSR_TX;
+        }
+
+        let mut byte = [0; 1];
+        let cloned_uart = uart.clone();
+        let cloned_interrupting = interrupting.clone();
+        let _uart_thread_for_read = thread::spawn(move || loop {
+            match io::stdin().read(&mut byte) {
+                Ok(_) => {
+                    let (uart, cvar) = &*cloned_uart;
+                    let mut uart = uart.lock().expect("failed to get an UART object");
+                    // Wait for the thread to start up.
+                    while (uart[(UART_LSR - UART_BASE) as usize] & UART_LSR_RX) == 1 {
+                        uart = cvar.wait(uart).expect("the mutex is poisoned");
+                    }
+                    uart[0] = byte[0];
+                    cloned_interrupting.store(true, Ordering::Release);
+                    // Data has been receive.
+                    uart[(UART_LSR - UART_BASE) as usize] |= UART_LSR_RX;
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+        });
+        Self { uart, interrupting }
     }
 
     fn load8(&mut self, addr: u64) -> u64 {
+        let (uart, cvar) = &*self.uart;
+        let mut uart = uart.lock().expect("failed to get an UART object");
         match addr {
             UART_RHR => {
-                self.lsr &= !UART_LSR_RX;
-                self.buffer as u64
+                cvar.notify_one();
+                uart[(UART_LSR - UART_BASE) as usize] &= !UART_LSR_RX;
+                uart[(UART_RHR - UART_BASE) as usize] as u64
             }
-            _ => 0,
+            _ => uart[(addr- UART_BASE) as usize] as u64,
         }
     }
 
     fn store8(&mut self, addr: u64, value: u64) {
+        let (uart, _cvar) = &*self.uart;
+        let mut uart = uart.lock().expect("failed to get an UART object");
         match addr {
             UART_THR => {
                 print!("{}", value as u8 as char);
                 io::stdout().flush().expect("failed to flush stdout");
             }
-            _ => {}
+            _ => {
+                uart[(addr- UART_BASE) as usize] = value as u8;
+            }
         }
     }
 }

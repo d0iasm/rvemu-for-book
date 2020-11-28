8b0317a01efd01d77a88173f8d5f516f365e5830
Asami Doi <doiasami1219@gmail.com>
2020-11-28 13:02:04 +0900

    Add cargo fmt to the test script.

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ projects="01 02 step03 step04 step05 step06 step07 step08 step09 step10"
 home=$(pwd)
 for project in $projects; do
     cd $project
+    cargo fmt
     cargo build
     cd $home
 done
